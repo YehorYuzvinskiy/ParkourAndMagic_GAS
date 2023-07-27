a4af3e578ef39b7e75dd41a682bd961438db30d4
Yehor <yehor.yuzvinskiy@gmail.com>
2023-07-27 17:49:21 +0300

    BlendSpace and AnimInstance class

---
+++ Source/ParkourAndMagic/AnimInstance/PAM_AnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PAM_AnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARKOURANDMAGIC_API UPAM_AnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
